83056ff367b5030cc070fdf44bad4ae958e335d8
Seul <seulee95@naver.com>
2020-07-24 23:22:13 +0900

    200724

---
+++ solvingStrategies/src/chapter8/Jlis.java
@@ -0,0 +1,99 @@
+package chapter8;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/** (Longest Increasing Subsequence) 최대 증가 부분 수열 **/
+public class Jlis { 
+	int[][] cache;
+	ArrayList<Integer> arrA;
+	ArrayList<Integer> arrB;
+
+	Jlis() {
+		this.cache = new int[101][101];
+		for(int[] arr : this.cache) {
+			Arrays.fill(arr, -1);
+		}
+		arrA = new ArrayList<Integer>();
+		arrB = new ArrayList<Integer>();
+	}
+	
+	private int solve(int aIdx, int bIdx) {
+		for(int i = 0; i < 6; i++) {
+			for (int j = 0; j < 6; j++) {
+				System.out.print(cache[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		if(cache[aIdx+1][bIdx+1] != -1) return cache[aIdx+1][bIdx+1];
+		cache[aIdx+1][bIdx+1] = 2;
+		long a, b, max;
+		a = aIdx == -1 ? Long.MIN_VALUE : arrA.get(aIdx);
+		b = bIdx == -1 ? Long.MIN_VALUE : arrB.get(bIdx);
+		max = Math.max(a, b);
+		for(int anIdx = aIdx+1; anIdx < arrA.size(); anIdx++) {
+			if(max < arrA.get(anIdx)) 
+				cache[aIdx+1][bIdx+1] = Math.max(cache[aIdx+1][bIdx+1], solve(anIdx, bIdx)+1);
+		}
+		for(int bnIdx = bIdx+1; bnIdx < arrB.size(); bnIdx++) {
+			if(max < arrB.get(bnIdx)) 
+				cache[aIdx+1][bIdx+1] = Math.max(cache[aIdx+1][bIdx+1], solve(aIdx, bnIdx)+1);
+		}
+		return cache[aIdx+1][bIdx+1];
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		for(int n = 0; n < N; n++) {
+			Jlis jlis = new Jlis();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int aN = Integer.parseInt(st.nextToken());
+			int bN = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine());
+			for(int a = 0; a < aN; a++) jlis.arrA.add(Integer.parseInt(st.nextToken()));
+			st = new StringTokenizer(br.readLine());
+			for(int b = 0; b < bN; b++) jlis.arrB.add(Integer.parseInt(st.nextToken()));
+			bw.append(jlis.solve(-1, -1) + "\n");
+		
+//			for(int i = 0; i < 8; i++) {
+//				for (int j = 0; j < 8; j++) {
+//					System.out.print(jlis.cache[i][j] + " ");
+//				}
+//				System.out.println();
+//			}
+			
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
+
+/* Input
+3
+3 3
+1 2 4
+3 4 7
+3 3
+1 2 3
+4 5 6
+5 3
+10 20 30 1 2
+10 20 30
+*/
+
+/* Output
+5
+6
+5
+ */
--- solvingStrategies/src/chapter8/WildCard.java
+++ solvingStrategies/src/chapter8/WildCard.java
@@ -5,36 +5,85 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/*
+ * 메모이제이션 사용 왜 효과적인 것인가...
+ */
 
 public class WildCard {
 	static String pattern;
-	static ArrayList<String> files;
+	static String file;
+	static ArrayList<String> fileList;
+	static int[][] cache;
+	static BufferedWriter bw;
 	
-	public void solve() {
+	public void solve() throws Exception{
+		cache = new int[101][101];
+		ArrayList<String> matchingFiles = new ArrayList<String>();
 		
+		for(int i = 0; i < fileList.size(); i++) {
+			for(int[] arr : cache) Arrays.fill(arr, -1); //캐시 초기화
+			file = fileList.get(i);
+			if(match(0, 0) == 1) matchingFiles.add(file);
+		}
+		
+		// 정답 정렬 및 버퍼 write
+		Collections.sort(matchingFiles);
+		for(int i = 0; i < matchingFiles.size(); i++) {
+			bw.append(matchingFiles.get(i));
+			bw.newLine();
+		}
 	}
 	
-	public void match() {
+	public int match(int pIdx, int fIdx) { //패턴 인덱스 , 파일 인덱스
+		if(cache[pIdx][fIdx] != -1) return cache[pIdx][fIdx]; //캐시 일치할 경우
 		
+		/** 인덱스가 길이를 벗어나지 않고 패턴 인덱스에 해당하는 캐릭터가 ?거나 파일이름 인덱스에 해당하는 캐릭터와 동일할 때 **/
+		if (pIdx < pattern.length() && fIdx < file.length() && (pattern.charAt(pIdx) == '?' || pattern.charAt(pIdx) == file.charAt(fIdx))) {
+			return cache[pIdx][fIdx] = match(pIdx+1, fIdx+1);
+		} 
+		// while문 대신 재귀를 사용했을 때 왜 시간 복잡도가 줄어들지? 띠용스
+		
+		/** 패턴 인덱스 끝날 때 파일 인덱스도 끝나면 1 반환 , 아니라면 0 반환 **/
+		if(pIdx == pattern.length()) 
+			return cache[pIdx][fIdx] = (fIdx == file.length()) ? 1 : 0;
+		
+		/**
+		 *  패턴 인덱스에 해당하는 캐릭터가 '*' 라면 
+		 * (1) 패턴 인덱스+1 캐릭터가 현재 파일 인덱스 캐릭터와 같은 경우  ex) r*ed vs red
+		 * (2) 파일 인덱스가 길이를 벗어나지 않고 패턴 인덱스 캐릭터와 파일 인덱스+1 캐릭터가 같은 경우 ex) r*ed vs rrrsdfred
+		 **/
+		if(pattern.charAt(pIdx) == '*') {
+			if(match(pIdx+1, fIdx) == 1 || (fIdx < file.length() && match(pIdx, fIdx+1) == 1))
+				return cache[pIdx][fIdx] = 1;
+		}
+		
+		return cache[pIdx][fIdx] = 0; //일치하지 않는 경우 0 반환
 	}
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		files = new ArrayList<String>();
+		fileList = new ArrayList<String>();
 		int N = Integer.parseInt(br.readLine());
 		
 		for(int i = 0; i < N; i++) {
 			pattern = br.readLine();
 			int fileNum = Integer.parseInt(br.readLine());
 			for(int j = 0; j < fileNum; j++) {
-				files.add(br.readLine());
+				fileList.add(br.readLine());
 			}
 //			System.out.println(files.toString());
 			new WildCard().solve();
-			files.clear();
+			fileList.clear();
 		}
+		
+		bw.flush();
+		br.close();
+		bw.close();
 	}
 
 }
